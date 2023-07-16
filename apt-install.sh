aaa08b98f7db4c847e5883659ff97fe452131a2a
Dan Murarasu <dan.murarasu@gmail.com>
2023-07-16 22:38:10 +0100

    adding palo alto pip modules and exporting env var for ansible config file

--- apt-install.sh
+++ apt-install.sh
@@ -22,3 +22,6 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
+# for ansible config file
+export ANSIBLE_CONFIG=/ansible
+
