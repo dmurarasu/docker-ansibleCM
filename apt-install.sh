e8e8a7a1c76aa973f690dc8512d80edaf756371e
Dan Murarasu <dan.murarasu@gmail.com>
2023-09-01 12:28:51 +0100

    format

--- apt-install.sh
+++ apt-install.sh
@@ -8,20 +8,5 @@ apt-get -yq install tzdata
 # set your timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 apt-get -yq install $(cat /tmp/requirements-apt.txt | tr '\n' ' ')
-# install pyenv
-curl https://pyenv.run | bash
-# pyenv shell env
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init -)"' >> ~/.profile
-# for zsh
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
-# for ansible config file
-export ANSIBLE_CONFIG=/ansible
 
--- pyenv-setup.sh
+++ pyenv-setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+# install pyenv
+curl https://pyenv.run | bash
+# pyenv shell env
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+# for zsh
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# for ansible config file
+export ANSIBLE_CONFIG=/ansible
+
 exec "$SHELL"
 
 ##install pyenv latest python
